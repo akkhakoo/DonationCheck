947db39a8154b56365fc35d2643d55ec73423d5c
akkhakoo <52111721+akkhakoo@users.noreply.github.com>
2019-06-25 19:33:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU3YzQ4OWRjLTIwMTktNGZhNi05NmMwLTAxYjQ0NzU1NGVlNyIsImlhdCI6MTU2MTIzMTMwOSwic3ViIjoiZGV2ZWxvcGVyLzEwMjM1ZTU1LTEwNzEtNWJmZi1mN2I2LWYyNzU5ZjlmZjMzNCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy43My4xNDguMjUyIl0sInR5cGUiOiJjbGllbnQifV19.8n2e6K3b8jTz9UhovEYKvdnqWtb2MckODnA-AYYkK6HNYj1zb8o8W3GOvSTVYdOig5a8xBlRBjUSnG3iflmw6Q',
+  discordToken: 'NTkyMDcyMjAyMzIxMzk1NzEz.XQ6aJA.5KEe6-0hWKoqLQfpCOxyNj-VuCQ',
+  clans: [
+    {
+      tag: '#8V82CQUP',
+      channelID: '592036106816454679',
+      color: 3447003
+    }
+  ],
+  timeDelay: 90,
+  showLeague: false,
+  useRichEmbed: true
+}
